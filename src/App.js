0d70d26d08244de8848029eb9c9a4fbffb140a8f
wagowsky <advakond@gmail.com>
2020-10-02 23:34:30 +0200

    fsdfs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import {Route} from "react-router-dom"
 import './App.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './components/HomeScreen';
+// import Products from './components/Products';
+import Product from './components/Product';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-     
-      </header>
-    </div>
+    <>
+      <Header/>
+      <main>
+        <Route exact path="/" component={HomeScreen}/>
+        <Route path="/product/:id" component={Product}/>
+        {/* <Route path="/cart/" component={Cart}/>
+        <Route path="/login/" component={Login}/> */}
+        
+      </main>
+      <Footer/>
+    </>
   );
 }
 
